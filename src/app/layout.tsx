5baaed6bea1c2bc25dbc3346b3dedc19fd5bde10
umarprogrammer19 <umarprogrammer19666@gmail.com>
2025-02-07 20:50:23 +0500

    Successfully Added Theme And Sidebar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,9 @@
-import type React from "react"
+import { Sidebar } from "@/components/sidebar"
+import { ThemeProvider } from "@/components/theme-provider"
 import type { Metadata } from "next"
 import { Inter } from "next/font/google"
+import type React from "react"
 import "./globals.css"
-// import { ThemeProvider } from "@/components/theme-provider"
-// import { Sidebar } from "@/components/sidebar"
 
 const inter = Inter({ subsets: ["latin"] })
 
@@ -20,12 +20,12 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={inter.className}>
-        {/* <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
+        <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
           <div className="flex h-screen">
             <Sidebar />
             <main className="flex-1 overflow-y-auto bg-background p-8">{children}</main>
           </div>
-        </ThemeProvider> */}
+        </ThemeProvider>
       </body>
     </html>
   )
