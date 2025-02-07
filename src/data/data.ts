2e30d48ba1d8b303da41804202219da3fd2a5330
umarprogrammer19 <umarprogrammer19666@gmail.com>
2025-02-07 21:12:49 +0500

    Data

---
+++ src/data/data.ts
@@ -0,0 +1,34 @@
+export const statuses = [
+    {
+        value: "pending",
+        label: "Pending",
+    },
+    {
+        value: "processing",
+        label: "Processing",
+    },
+    {
+        value: "success",
+        label: "Success",
+    },
+    {
+        value: "failed",
+        label: "Failed",
+    },
+]
+
+export const priorities = [
+    {
+        value: "low",
+        label: "Low",
+    },
+    {
+        value: "medium",
+        label: "Medium",
+    },
+    {
+        value: "high",
+        label: "High",
+    },
+]
+
