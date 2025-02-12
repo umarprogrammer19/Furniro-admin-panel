d642aab0c0eeaecb7d1d458192d64b7d5e082aee
umarprogrammer19 <umarprogrammer19666@gmail.com>
2025-02-12 23:14:38 +0500

    Optimized

--- src/app/products/post/page.tsx
+++ src/app/products/post/page.tsx
@@ -1,7 +1,16 @@
+"use client";
 import AddProductForm from "@/components/addProductForm";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 
 export default function AddProductPage() {
+    const router = useRouter();
+    useEffect(() => {
+        const token = localStorage.getItem("UFO_AUTH_TOKEN");
+        if (!token) router.push("/login");
+    }, []);
+
     return (
         <div className="container mx-auto p-6">
             <AddProductForm />
